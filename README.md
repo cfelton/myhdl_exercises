c1db725b0b6e9fad6ac942f8aafdecb79ed3fc16
Chris Felton <chris.felton@gmail.com>
2015-03-24 22:24:51 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+
+
+# Introduction
+This repository contains a collection of IPython notebooks
+with MyHDL exercises (mex).  These exercises can be used as 
+a learning tool to learn hardware description and MyHDL.
+
+## Getting Started
+First, clone the repository and run the ipython notebook in the 
+cloned workspace:
+
+    >> git clone https://bitbucket.org/cfelton/mex
+    >> cd mex
+    >> ipython notebook
+    
+Next, to run the exercises the *myhdl* and *myhdl_tools* packages 
+are required.  If the above cell did not execute without error the 
+packages need to be retrieved and the notebook kernel restarted.  
+Both packages can be installed via *pypi*:
+
+    >> pip install myhdl
+    >> pip install myhdl_tools
+    
+Or the latest can be retrieved from:
+
+    >> git clone https://github.com/jandecaluwe/myhdl
+    >> hg clone https://bitbucket.org/cfelton/myhdl_tools
+    
+To start IPython see the [directions here]()
+
+Once the packages are installed open the first notebook and 
+verify all the cells can run.
+
+## Exercises
+Other than the first exercise, each requires an HDL module to be
+defined and an existing test will verify if the HDL module is 
+working correctly or not.
+
+0. Getting started [00_mex_start_here]
+
+0. Defining a hardware module.  This exercise defines a basic
+   shift register  [01_mex_shifty]
+
+0. Interfaces
+
+
---
+++ test_01_mex.py
@@ -0,0 +1,81 @@
+
+from __future__ import division
+from __future__ import print_function
+
+import os
+from random import randint
+
+from myhdl import *
+from myhdl_tools import Clock, Reset
+
+from _z01 import m_shifty
+
+def test(mod):
+    clock = Clock(0)
+    reset = Reset(0, active=0, async=True)
+    load = Signal(bool(0))
+    lval = Signal(intbv(0, max=256, min=0))
+    obit = Signal(bool(0))
+    ival = randint(0, lval.max-1)
+
+    def _test():
+        tbdut = mod(clock, reset, load, lval, obit, ival=ival)
+        tbclk = clock.gen()
+
+        log = [(-1,0,0,0,0,) for _ in range(20)]
+        isht = Signal(intbv(0, max=256, min=0))
+
+        @always(clock.posedge)
+        def tbmon():
+            log.append((now(), load, lval, obit, mod.y,))
+            log.pop(0)
+            
+        @instance
+        def tbstim():
+            try:
+                isht.next = ival
+                yield reset.pulse(10)
+                yield clock.posedge
+
+                for ii in xrange(1000):
+                    assert isht[7] == obit
+                    isht.next = concat(isht[6:0], isht[7])
+                    yield clock.posedge
+
+                for ii in range(33):
+                    rr = randint(1, lval.max-1)
+                    lval.next = rr
+                    load.next = True
+                    isht.next = rr
+                    yield clock.posedge
+                    load.next = False  
+                    yield clock.posedge                  
+                    for ii in xrange(1000):
+                        assert isht[7] == obit
+                        isht.next = concat(isht[6:0], isht[7])
+                        yield clock.posedge
+                        
+                print("Test Successful")
+            except Exception, err:
+                yield delay(10)
+                print("Test Error")
+                print("Last %d clock cycles" % (len(log)))
+                print(" time    | load | lval | obit | shift (y)")
+                for ee in log:
+                    print("%8d | %4d | %04X | %4d | %04X" % ee)
+                print(err)
+
+            raise StopSimulation
+
+        return tbclk, tbdut, tbstim, tbmon
+
+    traceSignals.name = 'vcd/01_mex'
+    if os.path.isfile(traceSignals.name+'.vcd'):
+        os.remove(traceSignals.name+'.vcd')
+    
+    Simulation(traceSignals(_test)).run()
+    toVHDL(mod, clock, reset, load, lval, obit, ival=ival)
+    toVerilog(mod, clock, reset, load, lval, obit, ival=ival)
+
+if __name__ == '__main__':
+    test(m_shifty)
